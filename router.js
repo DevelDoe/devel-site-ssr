ab2682404d7a404c15aecc4b40f2a88249f9e489
develdoe <andreeray@live.com>
2018-10-25 00:15:12 +0200

    lazy

--- router.js
+++ router.js
@@ -7,7 +7,13 @@ export function createRouter() {
     return new Router({
         mode: 'history',
         routes: [
-            //...
+            {
+                path: '/',
+                component: () => import('./components/Home.vue')
+            }, {
+                path: '/item/:id',
+                component: () => import('./components/Item.vue')
+            }
         ]
     })
 }
