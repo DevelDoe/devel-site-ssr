1bc4bddcf0d7e1546ac62a4fd1ee2535a3fbe1d9
develdoe <andreeray@live.com>
2018-10-28 05:48:12 +0100

    added a config file for api url

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,10 @@
 import axios from 'axios'
+import config from '../../config'
 
 export const getPosts = ({ commit }) => {
-    return axios.get('https://jsonplaceholder.typicode.com/todos').then( res => {
+    return axios.get(`${config.api_url}`).then( res => {
         commit('setPosts', res.data)
     }).catch(err => {
-        
-    })
-}
-export const getPost = ({ commit }, id) => {
-    return axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then( res => {
-        commit('setPost', res.data)
-    }).catch(err => {
-        
+        console.log(err)
     })
 }
