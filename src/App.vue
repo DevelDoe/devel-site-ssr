b7879ba2ccb38ccd3e4cc303067f914adb7d421e
develdoe <andreeray@live.com>
2018-11-08 00:14:42 +0100

    filters

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
         
 
-        <header class="row header">
+        <header class="row headeri">
             <h1><a href="/" class="">log.develdevils</a></h1>
         </header>
         
@@ -12,36 +12,10 @@
         <router-view/>
         </transition>
 
-    
-        <footer>
-            <ul>
-                <li>ĕn-jĕnˈdər by DevelDoe | </li>
-                <li><a href="https://www.facebook.com/develdoe"> facebook</a> | </li>
-                <li><a href="https://www.github.com/develdoe"> github</a> | </li>
-                <li><a href="https://www.instagram.com/develdoe"> instagram</a> | </li>
-                <li><a href="http://andreeray.se"> andreeray.se</a></li>
-            </ul>
-        </footer>
         
     </div>
 </template>
 
 <style lang="scss">
-#app {
-    
-    .header {
-        padding: 10px;
 
-        h1 {
-            padding: 0;
-            margin:0;
-            font-size: 1rem;
-        }
-    }
-    footer {
-        li {
-            float: left;
-        }
-    }
-}
 </style>
